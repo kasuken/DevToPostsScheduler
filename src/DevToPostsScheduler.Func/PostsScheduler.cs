605d88d69e970dd88b849933d907137fdeb77f98
Emanuele Bartolesi <bartolesiemanuele@hotmail.com>
2020-01-10 14:11:42 +0100

    remove devto api key

--- src/DevToPostsScheduler.Func/PostsScheduler.cs
+++ src/DevToPostsScheduler.Func/PostsScheduler.cs
@@ -9,13 +9,13 @@ namespace DevToPostsScheduler.Func
     public static class PostsScheduler
     {
         [FunctionName("PostsScheduler")]
-        public static async Task Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
+        public static async Task Run([TimerTrigger("0 */1 * * * *")]TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"PostsScheduler function start at: {DateTime.Now}");
 
             var worker = new SchedulerRunner.Worker();
 
-            await worker.Run("tWc86Z4x5M9okdisNuFpnee3");
+            await worker.Run(Environment.GetEnvironmentVariable("DevToApiKey"));
 
             log.LogInformation($"PostsScheduler function finished at: {DateTime.Now}");
         }
--- src/DevToPostsScheduler.SchedulerRunner/Worker.cs
+++ src/DevToPostsScheduler.SchedulerRunner/Worker.cs
@@ -10,6 +10,7 @@ namespace DevToPostsScheduler.SchedulerRunner
 {
     public class Worker
     {
+        //Example: ##PublishedOn:2019-12-21 08:00AM##
         public const string PublishDateRegEx = "##PublishOn:(.*)##";
 
         public async Task Run(string token)
