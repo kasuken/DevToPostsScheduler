798a67bd95866c63521cf4b0e770d930415a482f
Emanuele Bartolesi <bartolesiemanuele@hotmail.com>
2021-12-02 08:35:43 +0100

    update to .NET 6 and Function 4 runtime

--- src/DevToPostsScheduler.Data/DevToAPIDataService.cs
+++ src/DevToPostsScheduler.Data/DevToAPIDataService.cs
@@ -1,11 +1,11 @@
-﻿using System;
+﻿using DevToPostsScheduler.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
-using DevToPostsScheduler.Models;
-using Newtonsoft.Json;
 
 namespace DevToPostsScheduler.Data
 {
@@ -37,23 +37,17 @@ namespace DevToPostsScheduler.Data
 
         public async Task<bool> PublishArticle(string accessToken, UnpublishedArticle article)
         {
-            article.Published = true;
-
             var client = new HttpClient();
 
             client.DefaultRequestHeaders.Add("api-key", accessToken);
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-            var myContent = JsonConvert.SerializeObject(article);
-
-            var content = new StringContent(myContent, Encoding.UTF8, "application/json");
-
-            //var buffer = System.Text.Encoding.UTF8.GetBytes(myContent);
-            //var byteContent = new ByteArrayContent(buffer);
+            article.Published = true;
+            var content = new StringContent(JsonConvert.SerializeObject(article), Encoding.UTF8, "application/json");
 
             var response = await client.PutAsync($"https://dev.to/api/articles/{article.Id}", content);
 
-            return true;
+            return response.IsSuccessStatusCode;
         }
     }
 }
--- src/DevToPostsScheduler.Func/PostsScheduler.cs
+++ src/DevToPostsScheduler.Func/PostsScheduler.cs
@@ -1,23 +1,89 @@
 using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
+using DevToPostsScheduler.Data;
+using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 
 namespace DevToPostsScheduler.Func
 {
-    public static class PostsScheduler
+    public class PostsScheduler
     {
-        [FunctionName("PostsScheduler")]
-        public static async Task Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
+        private readonly ILogger _logger;
+
+        //Example: ##ToPublishOn:2019-12-21 08:00AM##
+        private const string PublishDateRegEx = "##ToPublishOn:(.*)##";
+
+        public PostsScheduler(ILoggerFactory loggerFactory)
         {
-            log.LogInformation($"PostsScheduler function start at: {DateTime.Now}");
-
-            var worker = new SchedulerRunner.Worker();
-
-            await worker.Run(Environment.GetEnvironmentVariable("DevToApiKey"));
-
-            log.LogInformation($"PostsScheduler function finished at: {DateTime.Now}");
+            _logger = loggerFactory.CreateLogger<PostsScheduler>();
         }
+
+        [Function("PostsScheduler")]
+        public async Task Run([TimerTrigger("0 */5 * * * *")] MyInfo myTimer)
+        {
+            _logger.LogInformation($"PostsScheduler executed at: {DateTime.Now}");
+
+            var apiKey = Environment.GetEnvironmentVariable("DevToApiKey");
+
+            var dataService = new DevToAPIDataService();
+
+            var articles = await dataService.LoadDraftArticles(apiKey);
+
+            foreach (var article in articles)
+            {
+                var pubDateString = FindPublishedDateInText(article.BodyMarkdown);
+
+                if (pubDateString is { Length: > 0})
+                {
+                    var pubDate = DateTime.Parse(pubDateString);
+
+                    if (pubDate < DateTime.Now)
+                    {
+                        var item = articles.Where(c => c.Id == article.Id).FirstOrDefault();
+
+                        item.BodyMarkdown = RemovePublishedDateInText(article.BodyMarkdown);
+
+                        await dataService.PublishArticle(apiKey, item);
+                    }
+                }
+            }
+        }
+
+        private string FindPublishedDateInText(string markdown)
+        {
+            var regex = new Regex(PublishDateRegEx);
+            var m = regex.Match(markdown);
+            string date = m.Groups[1].ToString();
+
+            return date;
+        }
+
+        private string RemovePublishedDateInText(string markdown)
+        {
+            var regex = new Regex(PublishDateRegEx);
+            var m = regex.Match(markdown);
+
+            markdown = markdown.Replace(m.Value, "");
+
+            return markdown;
+        }
+    }
+
+    public class MyInfo
+    {
+        public MyScheduleStatus ScheduleStatus { get; set; }
+
+        public bool IsPastDue { get; set; }
+    }
+
+    public class MyScheduleStatus
+    {
+        public DateTime Last { get; set; }
+
+        public DateTime Next { get; set; }
+
+        public DateTime LastUpdated { get; set; }
     }
 }
